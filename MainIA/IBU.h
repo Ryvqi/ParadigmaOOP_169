2beeaa8836f29982fe77bad7be0ba43826fbff6c
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-22 17:04:01 +0700

    perubahan menyeluruh

--- MainIA/IBU.h
+++ MainIA/IBU.h
@@ -1,26 +1,25 @@
 #pragma once
 #ifndef IBU_H
 #define IBU_H
-#include <iostream>
-#include "ANAK.h"
+#include <iostream>;
+#include "anak.h"
 #include <vector>
 using namespace std;
 
 class ibu {
 public:
 	string nama;
-	vector<anak> daftar_anak;
+	vector<anak*> daftar_anak;
 
-	ibu(string pNama) : nama(pNama) {
+	ibu(string pNama) :nama(pNama) {
 		cout << "Ibu \"" << nama << "\" ada \n";
 	}
 	~ibu() {
-		cout << "Ibu \"" << nama << "\" tidak ada \n";
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
 	void tambahAnak(anak*);
 	void cetakAnak();
 };
-
 void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);
 }
